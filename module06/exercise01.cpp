a8f54925d863bb4e7551c22971b92c88bc912919
Binnur KURT <deepcloudlabs@gmail.com>
2025-08-20 11:11:49 +0300

    Create exercise01.cpp

---
+++ module06/exercise01.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+    public:
+    A() {
+        cout << "A's constructor..." << endl;
+    }
+    ~A() {
+        cout << "A's destructor..." << endl;
+    }
+};
+
+class B : public A {
+    public:
+    B() {
+        cout << "B's constructor..." << endl;
+    }
+    ~B() {
+        cout << "B's destructor..." << endl;
+    }
+};
+class C : public B {
+    public:
+    C() {
+        cout << "C's constructor..." << endl;
+    }
+    ~C() {
+        cout << "C's destructor..." << endl;
+    }
+};
+
+class D : public C {
+    public:
+    D() {
+        cout << "D's constructor..." << endl;
+    }
+    ~D() {
+        cout << "D's destructor..." << endl;
+    }
+};
+// D -> C -> B -> A
+int main() {
+    cout << "Application is just started..." << endl;
+    D *p = new D();
+    delete p;
+    cout << "Application is just completed..." << endl;
+    return 0;
+}
